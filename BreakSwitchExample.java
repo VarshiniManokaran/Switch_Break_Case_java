5a3815b96aef8fefeff9037d771bc15364f2f4e7
VarshiniManokaran <varshini6678@gmail.com>
2024-08-07 13:03:26 +0530

    Add files via upload

---
+++ BreakSwitchExample.java
@@ -0,0 +1,29 @@
+public class BreakSwitchExample {
+    public static void main(String[] args) {
+        
+        
+                int name = 5;
+                switch (name) {
+                    case 1:
+                        System.out.println("Ramya");
+                        break;
+                    case 2:
+                        System.out.println("Divya");
+                        break;
+                    case 3:
+                        System.out.println("Kavitha");
+                        break;
+                    case 4:
+                        System.out.println("Ram");
+                        break;
+                    case 5:
+                        System.out.println("Arjun");
+                        
+                    default:
+                        System.out.println("Name:" +5);
+                        break;
+                }
+            }
+        }
+    
+
